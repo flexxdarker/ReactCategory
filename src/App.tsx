89108b23078318aa72c426a188e7a4d39895955d
Artem0904 <moonportal708@gmail.com>
2024-09-09 18:16:25 +0300

    categoryPage

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import LoginPage from "./components/login/LoginPage.tsx";
+//import CategoriesPage from "./components/categories/CategoriesPage.tsx";
+import CategoryCreatePage from "./components/category/create";
+
+function App() {
+
+  return (
+      <>
+          {/*<LoginPage/>*/}
+          <CategoryCreatePage/>
+      </>
+  )
+}
+
+export default App
